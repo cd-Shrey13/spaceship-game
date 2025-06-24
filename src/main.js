19fecee55a51861cfbf168a1ec26a686464f2930
ShreyP. <shreyprajapati13@gmail.com>
2025-06-24 22:31:00 +0530

    handle collisions

--- src/main.js
+++ src/main.js
@@ -6,13 +6,17 @@ const windowWidth = window.visualViewport.width;
 
 const body = document.querySelector("body");
 
-body.addEventListener("touchmove", (e) => {
-  console.log(e);
-  spaceShip.style.left = `${e.targetTouches[0].clientX}px`;
-  spaceShip.style.top = `${e.targetTouches[0].clientY}px`;
+spaceShip.addEventListener("touchmove", (e) => {
+  const xCoords = e.targetTouches[0].clientX;
+  const yCoords = e.targetTouches[0].clientY;
+
+  if (50 < xCoords < window.innerWidth - 100)
+    spaceShip.style.left = `${e.targetTouches[0].clientX}px`;
+  // if (50 < yCoords < window.innerHeight - 100)
+  //   spaceShip.style.top = `${e.targetTouches[0].clientY}px`;
 });
 
-body.addEventListener("touchmove", (e) => launchFireshot(e));
+// body.addEventListener("touchmove", (e) => launchFireshot(e));
 
 function createObstacle() {
   const obstacle = document.createElement("img");
@@ -41,6 +45,7 @@ function moveObstacle(obstacle, obstacleTopPosition) {
     obstacle.remove();
   }
   requestAnimationFrame(() => {
+    detectCollision();
     moveObstacle(obstacle, obstacleTopPosition);
   });
 }
@@ -55,10 +60,23 @@ function createFireShot() {
   return fireshot;
 }
 
-function launchFireshot(e) {
+function createExplosion() {
+  const explosion = document.createElement("img");
+  explosion.src = "exposion.png";
+  explosion.setAttribute("class", "explosion");
+  explosion.setAttribute("id", "explosion");
+  explosion.style.position = "absolute";
+  return explosion;
+}
+
+function launchFireshot() {
   const fireshot = createFireShot();
-  const fireshotTopPosition = e.targetTouches[0].clientY;
-  fireshot.style.left = `${e.targetTouches[0].clientX}px`;
+  console.log();
+
+  const fireshotTopPosition = window.innerHeight - 150;
+  fireshot.style.left = `${Math.round(
+    Number.parseInt(spaceShip.style.left)
+  )}px`;
   fireshot.style.top = `${fireshotTopPosition}px`;
 
   body.appendChild(fireshot);
@@ -77,4 +95,40 @@ function moveFireshot(fireshot, fireshotTopPosition) {
   });
 }
 
+function detectCollision() {
+  const obstacles = document.querySelectorAll(".obstacle");
+  const fireshots = document.querySelectorAll(".fireshot");
+  for (let i = 0; i < obstacles.length; i++) {
+    for (let j = 0; j < fireshots.length; j++) {
+      // Simple bounding box collision detection
+      const obstacleRect = obstacles[i].getBoundingClientRect();
+      const fireshotRect = fireshots[j].getBoundingClientRect();
+      if (
+        obstacleRect.left < fireshotRect.right &&
+        obstacleRect.right > fireshotRect.left &&
+        obstacleRect.top < fireshotRect.bottom &&
+        obstacleRect.bottom > fireshotRect.top
+      ) {
+        // Collision detected, remove both
+        showExplosion(obstacleRect.right, obstacleRect.top);
+        obstacles[i].remove();
+        fireshots[j].remove();
+
+        return;
+      }
+    }
+  }
+}
+
+function showExplosion(left, top) {
+  const explosion = createExplosion();
+  explosion.style.left = `${left}px`;
+  explosion.style.top = `${top}px`;
+  body.appendChild(explosion);
+  setTimeout(() => {
+    explosion.remove();
+  }, 100);
+}
+
 setInterval(launchObstacle, 3000);
+setInterval(launchFireshot, 1000);
